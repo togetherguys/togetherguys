eba8789e24dbe533bdb60274e50f71261fbd991e
playboy <19837211812@163.com>
2024-09-13 17:03:30 +0800

    100 examples of Python
    3 homework of mine

---
+++ 12.py
@@ -0,0 +1,9 @@
+result=[]
+for i in range(101,201):
+    flag=0
+    for j in range(2,i):
+        if i%j==0:
+            flag=1
+    if flag==0:
+        result.append(i)
+print(len(set(result)))
---
+++ 53.py
@@ -0,0 +1,3 @@
+a=int(input())     #bin(5):0000 0101
+b=int(input())     #bin(3):0000 0011
+print(a&b,a|b,a^b,~a) #与、或、异或、取反。  
